a8c3bd8f9516be579191d32e459d772963f8c404
andre.ribeiro <ribeirolimand@gmail.com>
2023-04-27 00:31:12 -0300

    feature: prometheus

--- src/main/java/edu/br/infnet/controller/UserController.java
+++ src/main/java/edu/br/infnet/controller/UserController.java
@@ -3,12 +3,16 @@ package edu.br.infnet.controller;
 import edu.br.infnet.infrastructure.UserMapper;
 import edu.br.infnet.model.User;
 import edu.br.infnet.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+
+    final static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final UserService service;
 
@@ -20,11 +24,13 @@ public class UserController {
     public ResponseEntity<ResponseModel> get(@PathVariable Long cpf){
         try {
             UserMapper userMapper = service.fetch(cpf);
+            logger.info("User fetched: " + cpf);
             return new ResponseEntity<>(
                     ResponseModel.success(200, "OK", userMapper),
                     HttpStatus.OK
             );
         } catch (Exception error) {
+            logger.error("Error getting user with cpf: " + cpf);
             return new ResponseEntity<>(
                     ResponseModel.fail(404, error.getMessage()),
                     HttpStatus.NOT_FOUND
